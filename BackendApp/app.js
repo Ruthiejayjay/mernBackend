dbe7061e2cbb7cbd04ced395e7232695ff68471d
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-06-27 20:39:27 +0100

    added mongoose

--- BackendApp/app.js
+++ BackendApp/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -9,22 +10,28 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places',placesRoutes);
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes)
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route', 404);
-    throw error; 
+    throw error;
 });
 
-app.use((error,req, res, next) => {
-    if(req.headerSent) {
+app.use((error, req, res, next) => {
+    if (req.headerSent) {
         return next(error);
     }
     res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occurred'});
+    res.json({ message: error.message || 'An unknown error occurred' });
 });
 
 
-
-app.listen(5000);
+mongoose
+    .connect('mongodb+srv://RM:BTSleader@cluster0.vlfvq.mongodb.net/places?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
