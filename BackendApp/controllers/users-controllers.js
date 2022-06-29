0dcffd1992c86fa62ba3633496976d5672ba66cc
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-06-29 09:58:45 +0100

    connected backendto frontend

--- BackendApp/controllers/users-controllers.js
+++ BackendApp/controllers/users-controllers.js
@@ -94,7 +94,10 @@ const login = async (req, res, next) => {
         return next(error)
     }
 
-    res.json({ message: 'Logged in!!' })
+    res.json({ 
+      message: 'Logged in!!', 
+      user: existingUser.toObject({ getters: true}) 
+    })
 
 
 };
