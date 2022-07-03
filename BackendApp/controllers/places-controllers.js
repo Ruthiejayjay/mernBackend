523366ad6e4d940592e832d2b4b58e81d4e70ba9
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-07-03 21:33:30 +0100

    added authentication and authorization

--- BackendApp/controllers/places-controllers.js
+++ BackendApp/controllers/places-controllers.js
@@ -62,7 +62,7 @@ const createPlace = async (req, res, next) => {
     );
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -78,12 +78,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator
+    creator: req.userData.userId
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
 
   } catch (err) {
     const error = new HttpError(
@@ -140,6 +140,15 @@ const updatePlace = async (req, res, next) => {
     );
     return next(error)
   }
+
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError(
+      'You are not allowed edit this place.',
+      401
+    );
+    return next(error)
+  }
+
   place.title = title;
   place.description = description;
 
@@ -175,6 +184,14 @@ const deletePlace = async (req, res, next) => {
     const error = new HttpError(
       'Could not find place for this id',
       404
+    );
+    return next(error)
+  }
+
+  if(place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      "You're not allowed to delete this place",
+      401
     );
     return next(error)
   }
