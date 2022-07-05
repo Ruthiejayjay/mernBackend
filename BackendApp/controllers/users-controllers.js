1e1e43d0e9ae42e997d1f54026e75a0b082f8dd7
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-07-05 13:34:42 +0100

    deployed app to heroku

--- BackendApp/controllers/users-controllers.js
+++ BackendApp/controllers/users-controllers.js
@@ -83,7 +83,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
-      'supersecret_dont_share',
+      process.env.JWT_KEY,
       { expiresIn: '1h' }
     );
   } catch (err) {
@@ -146,7 +146,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      'supersecret_dont_share',
+      process.env.JWT_KEY,
       { expiresIn: '1h' }
     );
   } catch (err) {
